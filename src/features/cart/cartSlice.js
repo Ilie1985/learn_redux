f3e4d91233cc811a9353e4337e035e82b8894795
Ilie1985 <ilie.marian32@yahoo.com>
2023-02-03 15:51:47 +0000

    reducers- decrease

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { computeHeadingLevel } from "@testing-library/react";
 import cartItems from "../../cartItems";
 
 const initialState = {
@@ -23,15 +22,20 @@ const cartSlice = createSlice({
       });
     },
     increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+
+    decrease: (state, { payload }) => {
       const cartItem = state.cartItems.find((item) => {
         return item.id === payload.id;
       });
-      cartItem.amount = cartItem + 1;
+      cartItem.amount = cartItem.amount - 1;
     },
   },
 });
 // console.log("cartslice log",cartSlice);
 
-export const { clearCart, removeItem, increase } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
