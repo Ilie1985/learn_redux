0df9e297cc9327dfc87eb4bfe9fbed3b976075d2
Ilie1985 <ilie.marian32@yahoo.com>
2023-02-06 11:17:43 +0000

    modal complete

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
-  const { cartItems} = useSelector((store) => {
+  const { cartItems } = useSelector((store) => {
     return store.cart;
+  });
+
+  const { isOpen } = useSelector((store) => {
+    return store.modal;
   });
 
   const dispatch = useDispatch();
@@ -18,6 +23,7 @@ function App() {
 
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,7 +1,8 @@
 import React from "react";
 import CartItem from "./CartItem";
 import { useDispatch, useSelector } from "react-redux";
-import { clearCart } from "../features/cart/cartSlice";
+
+import {openModal} from "../features/modal/modalSlice"
 
 const CartContainer = () => {
   const dispatch = useDispatch();
@@ -43,7 +44,7 @@ const CartContainer = () => {
         <button
           className="btn clear-btn"
           onClick={() => {
-            dispatch(clearCart());
+            dispatch(openModal());
           }}
         >
           clear cart
